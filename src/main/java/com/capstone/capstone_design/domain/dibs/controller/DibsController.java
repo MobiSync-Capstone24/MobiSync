e70374ad7c8544967a036f57a7e850562a3f66e8
hoon <hoonluck3@naver.com>
2025-08-06 01:02:26 +0900

    feature: 찜하기 기능 구현.

--- src/main/java/com/capstone/capstone_design/domain/dibs/controller/DibsController.java
+++ src/main/java/com/capstone/capstone_design/domain/dibs/controller/DibsController.java
@@ -1,4 +1,25 @@
 package com.capstone.capstone_design.domain.dibs.controller;
 
-public class Ex {
+import com.capstone.capstone_design.domain.dibs.service.DibsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dibs")
+@RequiredArgsConstructor
+public class DibsController {
+
+    private final DibsService dibsService;
+
+    @PostMapping("/restaurants/{restaurantId}")
+    public ResponseEntity<Void> createDibs(@PathVariable Long restaurantId) {
+        Long userId = 1L;
+
+        dibsService.createDibs(userId, restaurantId);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/capstone/capstone_design/domain/dibs/repository/DibsRepository.java
+++ src/main/java/com/capstone/capstone_design/domain/dibs/repository/DibsRepository.java
@@ -1,4 +1,11 @@
 package com.capstone.capstone_design.domain.dibs.repository;
 
-public class Ex {
+import com.capstone.capstone_design.domain.dibs.model.Dibs;
+import com.capstone.capstone_design.domain.restaurant.model.Restaurant;
+import com.capstone.capstone_design.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DibsRepository extends JpaRepository<Dibs, Long> {
+
+    boolean existsUserAndRestaurant(User user, Restaurant restaurant);
 }
